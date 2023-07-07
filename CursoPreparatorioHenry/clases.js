0d1810f4ef0b3c1de118036076dc7592851db318
Danselix95 <thecellomen@hotmail.com>
2023-07-07 15:59:37 -0500

    actualizacion clases

--- CursoPreparatorioHenry/clases.js
+++ CursoPreparatorioHenry/clases.js
@@ -1,7 +1,7 @@
 // clases en javascript
 
 // al declarar una clase la primera letra del nombre de la clase siempre debe ir en mayuscula
-/* function Auto(puertas, color, marcas, año, ruedas) {
+function Auto(puertas, color, marcas, año, ruedas) {
     this.puertas = puertas,
     this.color = color,
     this.marca = marcas,
@@ -10,13 +10,17 @@
     this.informacion = function(){
         console.log('Este es un ' + this.marca + ' con ' + this.ruedas + ' ruedas')
     }
-} */
+}
 // la palabra clave new es una palabra reservada que nos permite tomar como plantilla la funcion que le proporcionemos para poder crear diferentes objetos de la misma clase.
+var miPrimerAuto = new Auto(2, 'Rojo', 'Ferrari', 2023, 4)
 
+/* console.log(miPrimerAuto)
+console.log(miPrimerAuto.marca)
+miPrimerAuto.informacion() */
 
 //      hay dos tipos de sintaxis para funciones: funcion constructora y expresion de clase
 
-class Auto{
+/* class Auto2{
     constructor(puertas, color, marcas, año, ruedas){
     this.puertas = puertas,
     this.color = color,
@@ -24,13 +28,31 @@ class Auto{
     this.año = año,
     this.ruedas = ruedas,
     this.informacion = function(){
-        console.log('Este es un ' + this.marca + ' con ' + this.ruedas + ' ruedas')
+        console.log('Este es un ' + this.marca + ' de color ' + this.color + ' y tiene ' + this.ruedas + ' ruedas')
     }
     }    
 }
 
-var miPrimerAuto = new Auto(2, 'Rojo', 'Ferrari', 2023, 4)
+var miSegundoAuto = new Auto2(4, 'Blanco', 'Fiat', 2015, 4);
 
-// console.log(miPrimerAuto)
-// console.log(miPrimerAuto.marca)
-miPrimerAuto.informacion()
+console.log(miSegundoAuto)
+miSegundoAuto.informacion() */
+
+
+// Palabra reservada "This" 
+
+class football{
+    constructor(jugador){
+        this.jugador = jugador
+    }
+
+    obtenerNombre() {
+        console.log(this.jugador)
+    }
+}
+
+var argentina = new football('Messi')
+var Brasil = new football('Pele')
+
+argentina.obtenerNombre();
+Brasil.obtenerNombre();
